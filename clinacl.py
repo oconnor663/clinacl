9e13a61dade8ef95f1b375ddb2c3b40ecdf304da
Jack O'Connor <oconnor663@gmail.com>
2015-07-05 22:46:23 -0400

    keybase command

--- clinacl.py
+++ clinacl.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+import base64
 import binascii
 import docopt
 import nacl.secret
@@ -7,6 +8,7 @@ import nacl.signing
 import nacl.utils
 from six import print_
 import sys
+import umsgpack
 
 __doc__ = """\
 Usage:
@@ -17,6 +19,7 @@ Usage:
     clinacl verifygen <signingkey>
     clinacl sign <signingkey>
     clinacl verify <verifykey>
+    clinacl keybase
 """
 
 
@@ -83,6 +86,21 @@ def verify(keyhex):
     sys.stdout.buffer.write(plainbytes)
 
 
+def keybase():
+    sig_base64 = sys.stdin.read()
+    sig_msgpack_bytes = base64.b64decode(sig_base64)
+    sig_obj = umsgpack.unpackb(sig_msgpack_bytes)
+    keybytes_tagged = sig_obj['body']['key']
+    # Keybase KIDs are type-tagged with two bytes at the front and
+    # one byte in the back.
+    keybytes = keybytes_tagged[2:34]
+    verifykey = nacl.signing.VerifyKey(keybytes)
+    detatched_sig_bytes = sig_obj['body']['sig']
+    sig_payload = sig_obj['body']['payload']
+    attached_sig_bytes = detatched_sig_bytes + sig_payload
+    sys.stdout.buffer.write(verifykey.verify(attached_sig_bytes))
+
+
 def main():
     args = docopt.docopt(__doc__)
     if args['secretgen']:
@@ -99,6 +117,8 @@ def main():
         sign(args['<signingkey>'])
     elif args['verify']:
         verify(args['<verifykey>'])
+    elif args['keybase']:
+        keybase()
 
 
 if __name__ == '__main__':
